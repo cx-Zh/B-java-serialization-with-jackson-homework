4b177287c86832864ab88a8d014242672cb7ac21
cx-Zh <491907188@qq.com>
2020-06-11 22:57:48 +0800

    finish functions

---
+++ src/main/java/com/thoughtworks/capability/gtb/vo/CustomLocalDateTimeDesSerializer.java
@@ -0,0 +1,28 @@
+package com.thoughtworks.capability.gtb.vo;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+class CustomLocalDateTimeDesSerializer extends StdDeserializer<LocalDateTime> {
+
+    protected CustomLocalDateTimeDesSerializer() {
+        this(null);
+    }
+
+    protected CustomLocalDateTimeDesSerializer(Class<LocalDateTime> t) {
+        super(t);
+    }
+
+    @Override
+    public LocalDateTime deserialize(JsonParser jsonparser, DeserializationContext context)
+            throws IOException {
+        Long timestamp = Long.parseLong(jsonparser.getText());
+        return LocalDateTime.ofInstant(Instant.ofEpochMilli(timestamp), ZoneId.systemDefault());
+    }
+}
--- src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
@@ -1,6 +1,13 @@
 package com.thoughtworks.capability.gtb.vo;
 
+
 import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,6 +24,10 @@ public class EventVo {
   private String id;
   private String name;
   private EventType type;
+
+  @JsonSerialize(using = CustomLocalDateTimeSerializer.class)
+  @JsonDeserialize(using = CustomLocalDateTimeDesSerializer.class)
   private LocalDateTime time;
+  @JsonUnwrapped
   private UserVo user;
 }
